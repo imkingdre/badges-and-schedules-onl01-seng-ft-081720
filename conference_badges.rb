e47a2af1aa37f3508ca6984cca36ed6b211ab07d
Andre King <andreking1031@gmail.com>
2020-08-19 14:53:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 def badge_maker(name)
   "Hello my name is #{name}"
 end
-def batch_badge_creator(speakers)
+def batch_badge_creator(arr)
+  
   speakers.collect do |name|
     badge_maker(name)
   end
