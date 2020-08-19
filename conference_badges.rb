625b954800905a1db47fc1d7232a4c5bb6ba8550
Andre King <andreking1031@gmail.com>
2020-08-19 14:58:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,7 @@ def batch_badge_creator(arr)
   return new_array
 end
 def assign_rooms(arr)
-  room = 0
-  speakers.collect do |name|
-    room += 1
-    "Hello, #{name}! You'll be assigned to room #{room}!"
+  
   end
 end
 def printer(speakers)
