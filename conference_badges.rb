f451a5332316480ce6e9e8e6ce9955d0b387eb8d
Andre King <andreking1031@gmail.com>
2020-08-19 14:57:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator(arr)
   arr.each do |name|
     new_array.push("Hello, my name is #{name}. ")
   end
-  return 
+  return new_array
 end
 def assign_rooms(speakers)
   room = 0
