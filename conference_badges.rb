872ccbc66a4b4c7d3fc20402870d359fc2f8fffe
Andre King <andreking1031@gmail.com>
2020-08-19 14:56:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,10 @@
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end
 def batch_badge_creator(arr)
   new_array = []
   arr.each do |name|
-    new_array.push("Hello, my name is #{name} ")
+    new_array.push("Hello, my name is #{name}. ")
   end
 end
 def assign_rooms(speakers)
